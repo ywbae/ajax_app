7ec00cf65f0a6e39898decd2cae63f009f2b0ec5
YongWan Bae <yonwan.b@gmail.com>
2020-08-18 15:36:00 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
   def checked
-    
     post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
